4de61a7ac17187336cd7a6b07a20ab4b98dfe120
Mark Tyneway <mark@purse.io>
2019-12-27 20:32:47 -0800

    cpu miner: start

--- src/powng-cpu.cc
+++ src/powng-cpu.cc
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include "common.h"
+#include "header.h"
+#include "share.h"
+
+// TODO: optimize this
 
 int32_t
 hs_cpu_run(
@@ -9,17 +13,17 @@ hs_cpu_run(
   uint32_t *result,
   bool *match
 ) {
-  // TODO: do we need a nonce here?
-  //uint32_t nonce = options->nonce;
+  // TODO: rewrite this to mine stuff
+  uint32_t nonce = options->nonce;
   uint32_t range = 1;
   size_t header_len = options->header_len;
-  uint8_t header[HEADER_SIZE];
+  uint8_t header[header_len];
   uint8_t hash[32];
   uint8_t chash[32];
 
   memset(hash, 0xff, 32);
 
-  if (header_len != HEADER_SIZE)
+  if (header_len != MINER_SIZE)
     return HS_EBADARGS;
 
   memcpy(header, options->header, header_len);
@@ -29,14 +33,14 @@ hs_cpu_run(
 
   bool has_sol = false;
 
+  hs_share_t *share = hs_share_alloc();
+  hs_share_decode(header, header_len, share);
+
   for (uint32_t r = 0; r < range; r++) {
     if (!options->running)
       break;
 
-    // TODO: fix this
-    /*
-    hs_pow_hash(header, header_len, chash);
-    */
+    hs_hash_share(share, chash);
 
     // Target must be share target
     if (memcmp(chash, options->target, 32) <= 0) {
